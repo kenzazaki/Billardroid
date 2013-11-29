c6eb84d630719747c4c0de5e2786e81b36af7ca0
TimmyCarbone <timmybox@live.fr>
2013-11-29 17:28:55 +0100

    Nouveau : menu, splashscreen, gestion des scènes

---
+++ src/com/project/billardroid/ResourcesManager.java
@@ -0,0 +1,142 @@
+package com.project.billardroid;
+
+import org.andengine.engine.Engine;
+import org.andengine.engine.camera.Camera;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder;
+import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder.TextureAtlasBuilderException;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.util.debug.Debug;
+
+import com.project.billardroid.GameActivity;
+
+/**
+ * @author Mateusz Mysliwiec
+ * @author www.matim-dev.com
+ * @version 1.0
+ */
+public class ResourcesManager
+{
+    //---------------------------------------------
+    // VARIABLES
+    //---------------------------------------------
+    
+    private static final ResourcesManager INSTANCE = new ResourcesManager();
+    
+    public Engine engine;
+    public GameActivity activity;
+    public Camera camera;
+    public VertexBufferObjectManager vbom;
+    public ITextureRegion splash_region;
+    private BitmapTextureAtlas splashTextureAtlas;
+    public ITextureRegion menu_background_region;
+    public ITextureRegion play_region;
+    public ITextureRegion options_region;
+        
+    private BuildableBitmapTextureAtlas menuTextureAtlas;
+    
+    //---------------------------------------------
+    // TEXTURES & TEXTURE REGIONS
+    //---------------------------------------------
+    
+    //---------------------------------------------
+    // CLASS LOGIC
+    //---------------------------------------------
+
+    public void loadMenuResources()
+    {
+        loadMenuGraphics();
+        loadMenuAudio();
+    }
+    
+    public void loadGameResources()
+    {
+        loadGameGraphics();
+        loadGameFonts();
+        loadGameAudio();
+    }
+    
+    private void loadMenuGraphics()
+    {
+    	BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+    	menuTextureAtlas = new BuildableBitmapTextureAtlas(activity.getTextureManager(), 1024, 1024, TextureOptions.BILINEAR);
+    	menu_background_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(menuTextureAtlas, activity, "splash.jpg");
+    	play_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(menuTextureAtlas, activity, "play_button.png");
+    	options_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(menuTextureAtlas, activity, "options_button.png");
+    	       
+    	try 
+    	{
+    	    this.menuTextureAtlas.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource, BitmapTextureAtlas>(0, 1, 0));
+    	    this.menuTextureAtlas.load();
+    	} 
+    	catch (final TextureAtlasBuilderException e)
+    	{
+    	        Debug.e(e);
+    	}
+    }
+    
+    private void loadMenuAudio()
+    {
+        
+    }
+
+    private void loadGameGraphics()
+    {
+        
+    }
+    
+    private void loadGameFonts()
+    {
+        
+    }
+    
+    private void loadGameAudio()
+    {
+        
+    }
+    
+    public void loadSplashScreen()
+    {
+    	BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+    	splashTextureAtlas = new BitmapTextureAtlas(activity.getTextureManager(), 800, 480, TextureOptions.BILINEAR);
+    	splash_region = BitmapTextureAtlasTextureRegionFactory.createFromAsset(splashTextureAtlas, activity, "splash.jpg", 0, 0);
+    	splashTextureAtlas.load();
+    }
+    
+    public void unloadSplashScreen()
+    {
+    	splashTextureAtlas.unload();
+    	splash_region = null;
+    }
+    
+    /**
+     * @param engine
+     * @param activity
+     * @param camera
+     * @param vbom
+     * <br><br>
+     * We use this method at beginning of game loading, to prepare Resources Manager properly,
+     * setting all needed parameters, so we can latter access them from different classes (eg. scenes)
+     */
+    public static void prepareManager(Engine engine, GameActivity activity, Camera camera, VertexBufferObjectManager vbom)
+    {
+        getInstance().engine = engine;
+        getInstance().activity = activity;
+        getInstance().camera = camera;
+        getInstance().vbom = vbom;
+    }
+    
+    //---------------------------------------------
+    // GETTERS AND SETTERS
+    //---------------------------------------------
+    
+    public static ResourcesManager getInstance()
+    {
+        return INSTANCE;
+    }
+}
